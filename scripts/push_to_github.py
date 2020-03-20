a43c0b7f13df740dc7645f2be3a477b10b6aed00
Andrei Pradan <andrei.pradan@gmail.com>
2020-03-20 11:28:36 +0200

    Auto commit

---
+++ scripts/push_to_github.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+from git import Repo
+
+PATH_OF_GIT_REPO = os.getenv('PATH_TO_GIT_FOLDER')
+
+
+def git_push(commit_message):
+    repo = Repo(PATH_OF_GIT_REPO)
+    repo.git.add(update=True)
+    repo.index.commit(commit_message)
+    origin = repo.remote(name='origin')
+    origin.push()
+
+
+if __name__ == '__main__':
+    message = sys.argv[1] if len(sys.argv) == 2 else 'Auto commit'
+    git_push(message)
