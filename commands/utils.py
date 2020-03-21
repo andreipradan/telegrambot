d3a98109bd6ca8a77e248202b33405f3128f2019
Andrei Pradan <andrei.pradan@gmail.com>
2020-03-21 03:31:09 +0200

    formatting

--- commands/utils.py
+++ commands/utils.py
@@ -6,4 +6,4 @@ def parse_country(data):
     items = list(data.items())
     return '\n├ '.join(
         [f'{key}: {value}' for key, value in items[:-1]]
-    ) + f'└{items[-1][0]}: {items[-1][1]}'
+    ) + f'\n└{items[-1][0]}: {items[-1][1]}'
