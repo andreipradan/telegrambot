2ed64bee390acb12f4992c72923fc0dbf6fe279e
Andrei Pradan <andrei.pradan@gmail.com>
2020-03-21 04:47:19 +0200

    refa

--- commands/utils.py
+++ commands/utils.py
@@ -5,7 +5,7 @@ def get_records_from_db(collection):
 def parse_country(data):
     items = list(data.items())
     return '\n├ '.join(
-        [f'{key}: {value}' for key, value in items[1:-1]]
+        [f'{key}: {value}' for key, value in items[:-1]]
     ) + f'\n└{items[-1][0]}: {items[-1][1]}'
 
 
@@ -27,5 +27,5 @@ Covid Global Stats
 ├{parse_country(top_stats)}
 {parse_countries(countries)}
 
-({last_updated}) [Source: {'DB' if from_db else 'worldometers.info/'}]
+({last_updated}) [Source: {'DB' if from_db else 'worldometers.info'}]
 """
