0305a6c532df8d66ce2a75a94a2933979bcff446
Andrei Pradan <andrei.pradan@gmail.com>
2020-03-23 01:22:32 +0200

    ref

--- core/ptb.py
+++ core/ptb.py
@@ -155,7 +155,6 @@ def end(update, bot):
     """Returns `ConversationHandler.END`, which tells the
     ConversationHandler that the conversation is over"""
     query = update.callback_query
-    bot = context.bot
     bot.edit_message_text(
         chat_id=query.message.chat_id,
         message_id=query.message.message_id,
