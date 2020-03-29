e418ac4e6ea61d7c87949c239c9913627ebdc114
Andrei Prădan <andrei.pradan@gmail.com>
2020-03-29 22:56:37 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -42,5 +42,5 @@ if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 8080)),
-        debug=os.getenv("DEBUG", True),
+        debug=os.getenv("DEBUG", False),
     )
