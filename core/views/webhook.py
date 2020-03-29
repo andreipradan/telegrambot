60381ba182c42813eb14bfadba35a439360bd309
Andrei Pradan <andrei.pradan@gmail.com>
2020-03-29 12:39:32 +0300

    done with webhook unit tests

--- core/views/webhook.py
+++ core/views/webhook.py
@@ -17,76 +17,62 @@ webhook_views = Blueprint("webhook_views", __name__)
 
 @webhook_views.route(f"/{constants.TOKEN}", methods=["POST"])
 def webhook():
-    if request.method == "POST":
-        json = request.get_json()
-        if not json:
-            raise ValueError("No payload")
+    json = request.get_json()
+    if not json:
+        raise ValueError("No payload")
 
-        bot = telegram.Bot(token=constants.TOKEN)
-        update = telegram.Update.de_json(json, bot)
-
-        if update.callback_query:
-            data = update.callback_query.data
-            if data == "end":
-                return inline.end(update)
-            elif data == "more":
-                return inline.more(update)
-            elif data == "back":
-                return inline.back(update)
-            return inline.refresh_data(update, getattr(scrapers, data)())
-
-        command_text, status_code = handlers.validate_components(update)
-
-        if status_code == 1337:
-            if command_text == "skip-debug":
-                return "ok"
-            text = f"{command_text}.\nUpdate: {update.to_dict()}"
-            return utils.send_message(bot, text=text)
-
-        chat_id = update.message.chat.id
-        if status_code == "reply_to_message":
-            return utils.send_message(
-                bot,
-                text=utils.parse_sentiment(
-                    scrapers.analyze_sentiment(command_text, json=True)
-                ),
-                chat_id=chat_id,
-            )
-
-        if status_code != "valid-command":
-            return utils.send_message(bot, text=command_text, chat_id=chat_id)
-
-        args = []
-        if command_text in constants.COMMANDS_WITH_TEXT:
-            args.append(" ".join(update.message.text.split(" ")[1:]))
-        elif command_text in constants.COMMANDS_WITH_UPDATE:
-            args.append(update)
-
-        if command_text == "start":
-            return inline.start(update)
-
-        results = getattr(scrapers, command_text)(*args)
-
-        try:
-            utils.send_message(bot, text=results, chat_id=chat_id)
-        except telegram.error.BadRequest as err:
-            text = (
-                f"Restrict results (e.g. /{command_text} 3"
-                if "Message is too long" in str(err)
-                else str(err)
-            )
-            utils.send_message(bot, text=text, chat_id=chat_id)
-        return "completed"
-    return f"Unexpected method {request.method}"
-
-
-@webhook_views.route(f"/reset-webhook/")
-def reset_webhook():
     bot = telegram.Bot(token=constants.TOKEN)
-    if request.args.get("key", None) != "@aoleu_bot":
-        abort(403)
-    bot.deleteWebhook()
-    url = url_for("webhook_views.webhook", _external=True).replace(
-        "http://", "https://"
-    )
-    return f"Result: {bot.setWebhook(url)}"
+    update = telegram.Update.de_json(json, bot)
+
+    if update.callback_query:
+        data = update.callback_query.data
+        if data == "end":
+            return inline.end(update)
+        elif data == "more":
+            return inline.more(update)
+        elif data == "back":
+            return inline.back(update)
+        return inline.refresh_data(update, getattr(scrapers, data)())
+
+    command_text, status_code = handlers.validate_components(update)
+
+    if status_code == 1337:
+        if command_text == "skip-debug":
+            return "ok"
+        text = f"{command_text}.\nUpdate: {update.to_dict()}"
+        return utils.send_message(bot, text=text)
+
+    chat_id = update.message.chat.id
+    if status_code == "reply_to_message":
+        return utils.send_message(
+            bot,
+            text=utils.parse_sentiment(
+                scrapers.analyze_sentiment(command_text, json=True)
+            ),
+            chat_id=chat_id,
+        )
+
+    if status_code != "valid-command":
+        return utils.send_message(bot, text=command_text, chat_id=chat_id)
+
+    args = []
+    if command_text in constants.COMMANDS_WITH_TEXT:
+        args.append(" ".join(update.message.text.split(" ")[1:]))
+    elif command_text in constants.COMMANDS_WITH_UPDATE:
+        args.append(update)
+
+    if command_text == "start":
+        return inline.start(update)
+
+    results = getattr(scrapers, command_text)(*args)
+
+    try:
+        utils.send_message(bot, text=results, chat_id=chat_id)
+    except telegram.error.BadRequest as err:
+        text = (
+            f"Restrict results (e.g. /{command_text} 3"
+            if "Message is too long" in str(err)
+            else str(err)
+        )
+        utils.send_message(bot, text=text, chat_id=chat_id)
+    return "completed"
--- tests/core/views/test_webhook.py
+++ tests/core/views/test_webhook.py
@@ -1,26 +1,29 @@
 from unittest import mock
 
 import pytest
+import telegram
 from flask import url_for
+
+from core import constants
 
 
 @mock.patch("telegram.Bot", return_value="Bot_foo")
+@mock.patch("telegram.Update.de_json")
 class TestWebhook:
     view_name = "webhook_views.webhook"
 
     @pytest.mark.parametrize("method", ["get", "put", "delete", "head"])
-    def test_with_not_allowed_methods(self, _, client, method):
+    def test_with_not_allowed_methods(self, _, __, client, method):
         method = getattr(client, method)
         response = method(url_for(self.view_name))
         assert response.status_code == 405
 
-    def test_no_json_in_payload(self, _, client):
+    def test_no_json_in_payload(self, _, __, client):
         with pytest.raises(ValueError) as e:
             client.post(url_for(self.view_name))
         assert e.value.args[0] == "No payload"
 
     @pytest.mark.parametrize("inline_func", ["end", "back", "more"])
-    @mock.patch("telegram.Update.de_json")
     def test_callback_query_end(self, json_mock, _, client, inline_func):
         json_mock.return_value.callback_query.data = inline_func
 
@@ -34,8 +37,7 @@ class TestWebhook:
 
     @mock.patch("scrapers.date_la_zi")
     @mock.patch("core.inline.refresh_data", return_value="refresh_data")
-    @mock.patch("telegram.Update.de_json")
-    def test_refresh_data(self, de_json, _, __, ___, client):
+    def test_refresh_data(self, _, __, de_json, ___, client):
         de_json.return_value.callback_query.data = "date_la_zi"
 
         response = client.post(url_for(self.view_name), json={"1": 2})
@@ -43,7 +45,6 @@ class TestWebhook:
         assert response.status_code == 200
         assert response.data.decode("utf-8") == "refresh_data"
 
-    @mock.patch("telegram.Update.de_json")
     @mock.patch("core.handlers.validate_components")
     def test_1337_status_skip_debug(self, validate, update, _, client):
         validate.return_value = "skip-debug", 1337
@@ -54,17 +55,100 @@ class TestWebhook:
         assert response.data.decode("utf-8") == "ok"
 
     @mock.patch("core.utils.send_message")
-    @mock.patch("telegram.Update.de_json")
     @mock.patch("core.handlers.validate_components")
-    def test_1337_status_debug(self, validate, update, message, _, client):
+    def test_1337_status_debug(self, validate, message, update, _, client):
         message.return_value = "send_foo"
         validate.return_value = "command_text_foo", 1337
         update.return_value.callback_query = None
 
         response = client.post(url_for(self.view_name), json={"1": 2})
-        validate.assert_called_once_with(mock.ANY)
+        validate.assert_called_once_with(update())
         message.assert_called_with(
             "Bot_foo", text=f"command_text_foo.\nUpdate: {update().to_dict()}"
         )
         assert response.status_code == 200
         assert response.data.decode("utf-8") == "send_foo"
+
+    @mock.patch("scrapers.analyze_sentiment", return_value="analyze_foo")
+    @mock.patch("core.utils.parse_sentiment", return_value="parse_foo")
+    @mock.patch("core.handlers.validate_components")
+    @mock.patch("core.utils.send_message", return_value="send_foo")
+    def test_reply_to_message(
+        self, message, validate, parse, analyze, update, __, client
+    ):
+        validate.return_value = "command_text_foo", "reply_to_message"
+        update.return_value.callback_query = None
+        update.return_value.message.chat.id = "foo_chat_id"
+
+        response = client.post(url_for(self.view_name), json={"1": 2})
+        validate.assert_called_once_with(update())
+        message.assert_called_once_with(
+            "Bot_foo", text="parse_foo", chat_id="foo_chat_id"
+        )
+        analyze.assert_called_once_with("command_text_foo", json=True)
+        parse.assert_called_once_with(analyze())
+        assert response.status_code == 200
+        assert response.data.decode("utf-8") == "send_foo"
+
+    @mock.patch("core.utils.send_message", return_value="send_foo")
+    @mock.patch("core.handlers.validate_components")
+    def test_invalid_command(self, validate, message, update, _, client):
+        validate.return_value = "command_text_foo", "foo_command"
+        update.return_value.callback_query = None
+
+        response = client.post(url_for(self.view_name), json={"1": 2})
+        validate.assert_called_once_with(update())
+        message.assert_called_once_with(
+            "Bot_foo",
+            text="command_text_foo",
+            chat_id=update().message.chat.id,
+        )
+        assert response.status_code == 200
+        assert response.data.decode("utf-8") == "send_foo"
+
+    @pytest.mark.parametrize("cmd", constants.COMMANDS_WITH_TEXT)
+    @mock.patch("core.utils.send_message", return_value="send_foo")
+    @mock.patch("core.handlers.validate_components")
+    def test_commands_with_text(self, validate, msg, update, _, cmd, client):
+        validate.return_value = cmd, "valid-command"
+        update.return_value.callback_query = None
+
+        with mock.patch(f"scrapers.{cmd}", return_value="cmd_foo"):
+            response = client.post(url_for(self.view_name), json={"1": 2})
+
+        validate.assert_called_once_with(update())
+        msg.assert_called_once_with(
+            "Bot_foo", text="cmd_foo", chat_id=update().message.chat.id,
+        )
+
+        assert response.status_code == 200
+        assert response.data.decode("utf-8") == "completed"
+
+    @mock.patch("core.inline.start", return_value="inline_start_foo")
+    @mock.patch("core.handlers.validate_components")
+    def test_start(self, validate, start, update, _, client):
+        validate.return_value = "start", "valid-command"
+        update.return_value.callback_query = None
+
+        response = client.post(url_for(self.view_name), json={"1": 2})
+
+        start.assert_called_once_with(update())
+        validate.assert_called_once_with(update())
+
+        assert response.status_code == 200
+        assert response.data.decode("utf-8") == "inline_start_foo"
+
+    @mock.patch("core.utils.send_message", side_effect=telegram.error.BadRequest("foo"))
+    @mock.patch("scrapers.date_la_zi", return_value="scrapers_foo")
+    @mock.patch("core.handlers.validate_components")
+    def test_message_too_long(self, validate, scrapers, message, update, _, client):
+        validate.return_value = "date_la_zi", "valid-command"
+        update.return_value.callback_query = None
+
+        with pytest.raises(telegram.error.BadRequest) as e:
+            client.post(url_for(self.view_name), json={"1": 2})
+
+        assert e.value.message == "foo"
+
+        validate.assert_called_once_with(update())
+        scrapers.assert_called_once_with()
