3ef7cfb904d2eac3c4370530656af251cc6e8cf8
Andrei Pradan <andrei.pradan@gmail.com>
2020-03-30 21:17:19 +0300

    scrapers 2nd att

--- scrapers/main.py
+++ scrapers/main.py
@@ -1,6 +1,12 @@
+import base64
+import json
 import logging
+import os
 
 
-def dispatcher(*args, **kwargs):
+def dispatcher(event, context):
     logger = logging.getLogger(__name__)
-    logger.info(f"args: {args}\nkwargs: {kwargs}")
+    logger.info(f"event: {event} | context{context}")
+
+    payload = json.loads(base64.b64decode(event["data"]).decode("utf-8"))
+    logger.info(f"payload: {payload}")
