2247d245ce4d57aa0aa91355599abed49c2e9e91
Andrei Pradan <andrei.pradan@gmail.com>
2020-04-05 23:08:11 +0300

    added homepage redirect to telegram bot page

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,7 @@ import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 from flask import Flask
+from flask import redirect
 
 from core.views.new_cases import new_cases_views
 from core.views.webhook import webhook_views
@@ -16,6 +17,11 @@ if not os.getenv("FLASK_DEBUG", False):
 app = Flask(__name__)
 app.register_blueprint(new_cases_views)
 app.register_blueprint(webhook_views)
+
+
+@app.route("/")
+def home():
+    return redirect("https://telegram.me/alfred_the_robot")
 
 
 if __name__ == "__main__":
